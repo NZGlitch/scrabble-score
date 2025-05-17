45808ac07771054208bc929170f4f6711ac7d5c6
Chris Noldus <NZGlitch@users.noreply.github.com>
2025-05-17 14:24:31 +1200

    Add bootstrap to head

--- includes/admin/head.php
+++ includes/admin/head.php
@@ -1,2 +1,20 @@
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scrabble Score Admin</title>
+
+    <!-- Bootstrap 5 CSS -->
+    <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+        crossorigin="anonymous"
+    >
+
+    <!-- Bootstrap 5 JavaScript (includes Popper) -->
+    <script 
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+Y4nFZB0a0vYjUec4tN6yhzR1tPp6" 
+        crossorigin="anonymous"
+    ></script>
 </head>
